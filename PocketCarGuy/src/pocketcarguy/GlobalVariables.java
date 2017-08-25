0125e886895eb55c2f4deefb04814147ddd7ed0a
Brandyn <brandynabel@gmail.com>
2017-08-24 22:43:47 -0500

    Added a couple of test cases, and made FloatDelta global

--- PocketCarGuy/src/pocketcarguy/GlobalVariables.java
+++ PocketCarGuy/src/pocketcarguy/GlobalVariables.java
@@ -5,7 +5,7 @@ public class GlobalVariables {
     //Celcius to farenheight times by multiplier plus addition
     public static float CToFMultiplier = 1.8f;
     public static float CToFAddition = 32.0f;
-    
+    public static float FloatDelta = 0.0001f;
     
     
     
