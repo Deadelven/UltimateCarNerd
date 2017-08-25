4a87619f5f75ddf7f931dcbf04aaeaaf30b16482
Brandyn <brandynabel@gmail.com>
2017-08-24 22:15:32 -0500

    Converted methods to static.
    - Removed some unnecessary variables
    - Added Static modifier to method signatures

--- PocketCarGuy/src/pocketcarguy/TemperatureConvertor.java
+++ PocketCarGuy/src/pocketcarguy/TemperatureConvertor.java
@@ -2,34 +2,27 @@
 package pocketcarguy;
 
 public class TemperatureConvertor {
-    float CelciusInput;
-    float FarenheightInput;
-    
-    float CelciusOutput;
-    float FarenheightOutput;
-    
+  
     
     //Convert Farenheight to celcius
-    public float ConvertToCelcius(float F)
+    public static float ConvertToCelcius(float F)
     {
         if(F == 0)
-        {CelciusOutput = -17.78f;}
+        {return -17.78f;}
         else
         {        
-            CelciusOutput = ((( F - GlobalVariables.CToFAddition) * 5) / 9 );
+            return ((( F - GlobalVariables.CToFAddition) * 5) / 9 );
         }
-        return CelciusOutput;
     }
     
-    public float ConvertToFarenheight(float C)
+    public static float ConvertToFarenheight(float C)
     {
         if(C == 0)
-        {FarenheightOutput = GlobalVariables.CToFAddition;}
+        {return GlobalVariables.CToFAddition;}
         else
         {        
-        FarenheightOutput = (C * GlobalVariables.CToFMultiplier) + GlobalVariables.CToFAddition;
+            return (C * GlobalVariables.CToFMultiplier) + GlobalVariables.CToFAddition;
         }
-        return FarenheightOutput;
     }
     
     
