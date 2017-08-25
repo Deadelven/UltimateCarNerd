9a946dbac7acd259733300ae5432905f829b6c42
Deadelven <kdh1995-beta@hotmail.co.uk>
2017-08-26 01:34:52 +1000

    Added Test for Weight Conversion Tests for KG to LB passed

--- PocketCarGuy/src/pocketcarguy/GlobalVariables.java
+++ PocketCarGuy/src/pocketcarguy/GlobalVariables.java
@@ -6,9 +6,13 @@ public class GlobalVariables {
     public static float CToFMultiplier = 1.8f;
     public static float CToFAddition = 32.0f;
     public static float FloatDelta = 0.0001f;
- 
+    //KW and HP Convertor
     public static float KWToHPMultiplier = 1.3410f;
     public static float HPToKWMultiplier = 0.7457f;
+    //KG and LB conversion
+    public static float KGToLBMultiplier = 2.205f;
+    public static float LBToKGMultiplier = 0.4536f;
+    
     
     
     
