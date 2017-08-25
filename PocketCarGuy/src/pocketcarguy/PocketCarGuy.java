b39cc0dec8fc8bf8dd2f33bb639cd8c2cd098a1e
DESKTOP-2PHKRV7\Deadelven <deadelvengamer@gmail.com>
2017-08-25 12:45:40 +1000

    Added Netbeans Project File

---
+++ PocketCarGuy/src/pocketcarguy/PocketCarGuy.java
@@ -0,0 +1,20 @@
+
+
+package pocketcarguy;
+
+
+
+public class PocketCarGuy {
+    
+
+    TemperatureConvertor TempConv = new TemperatureConvertor();
+    float CelciusOutput = TempConv.ConvertToCelcius(25);
+    float FarenheightOutput = TempConv.ConvertToFarenheight(12);
+    
+
+
+
+
+
+
+}
