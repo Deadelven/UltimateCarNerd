eefb8eadfcc412b893f608f9c279e864547dad0d
Deadelven <kdh1995-beta@hotmail.co.uk>
2017-08-27 18:09:32 +1000

    dditions

--- SpringBootCarGuy/src/main/java/hello/Greeting.java
+++ SpringBootCarGuy/src/main/java/hello/Greeting.java
@@ -10,5 +10,19 @@ package hello;
  * @author Deadelven
  */
 public class Greeting {
-    
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }    
 }
